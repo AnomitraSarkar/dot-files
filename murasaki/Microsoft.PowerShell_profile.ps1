34111dab10c455c683dca8a1591fdce07aa6754a
AnomitraSarkar <anomitrasarkar05@gmail.com>
2025-09-21 11:47:10 +0530

    murasaki

---
+++ murasaki/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,13 @@
+﻿
+function prompt{
+  $path = $(Get-Location).Path
+  $path = $path.ToLower() -replace '^([a-z]):', '/$1' -replace '\\', '/'
+  Write-Host ""
+  Write-Host $path -ForegroundColor Green
+  Write-Host "|> " -NoNewLine -ForegroundColor Green
+}
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\velvet.omp.json" | Invoke-Expression
+komorebic start --bar --whkd
+cls
+winfetch
+
